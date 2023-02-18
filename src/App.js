c2ad00dbbb24460bdad33b27739f2c24440c3644
Roman Portianko <rportyanko10@gmail.com>
2023-02-18 13:55:28 +0200

    Task with time

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <DateComponent />
+    </>
+  );
+}
+
+function DateComponent() {
+  let [time, setTime] = useState(new Date().toLocaleTimeString());
+  const clickHandler = () => {
+    setTime(new Date().toLocaleTimeString());
+  }
+  return (
+    <>
+      <h1>{time}</h1>
+      <button onClick={clickHandler}>Refresh time</button>
+    </>
   );
 }
 
