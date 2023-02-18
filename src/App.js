3b3c8fda636f2acbf74003330fdd6ce811cfd7b0
Roman Portianko <rportyanko10@gmail.com>
2023-02-18 14:04:19 +0200

    Fibonacci task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <>
-      <DateComponent />
+      <FibonacciComponent />
     </>
   );
 }
 
-function DateComponent() {
-  let [time, setTime] = useState(new Date().toLocaleTimeString());
-  const clickHandler = () => {
-    setTime(new Date().toLocaleTimeString());
-  }
-  return (
-    <>
-      <h1>{time}</h1>
-      <button onClick={clickHandler}>Refresh time</button>
-    </>
-  );
+function FibonacciComponent() {
+    const getFibonacci = n => {
+        if (n <= 1) {
+            return 1;
+        }
+        return getFibonacci(n - 1) + getFibonacci(n - 2);
+    };
+    const nextFibonacci = () => {
+        setFibonacci(fibonacci + 1);
+    };
+    const previousFibonacci = () => {
+        if (fibonacci > 0) {
+          setFibonacci(fibonacci - 1);
+        }
+        else {
+          alert("You are at the beginning of the Fibonacci sequence.")
+        }
+    };
+    let [fibonacci, setFibonacci] = useState(0);
+    return (
+        <>
+            <h1>Current Fibonacci: {getFibonacci(fibonacci)}</h1>
+            <button onClick={previousFibonacci}>Previous Fibonacci</button>
+            <button onClick={nextFibonacci}>Next Fibonacci</button>
+        </>
+    );
 }
 
 export default App;
+
